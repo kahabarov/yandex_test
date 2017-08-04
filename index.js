b6140b6b1deff1fa7eaeb8f84b6e72326b59a10e
kahabarov <khabarov.k.a@yandex.ru>
2017-08-04 18:00:37 +0300

    Решение проверено на локальном компьютере в Firefox и Chrome

---
+++ index.js
@@ -0,0 +1,180 @@
+const MyForm = (function(formId) {
+  // Приватные функции и данные
+  const formFields = ['fio', 'email', 'phone'];
+
+  const countWordsInFio = 3;
+
+  const validDomains = ["ya.ru", "yandex.ru", "yandex.ua", "yandex.by", "yandex.kz", "yandex.com"];
+
+  const maxSumOfDigits = 30;
+  // Точка вариации ТЗ
+  const ajaxRequestMethod = "GET"; // "POST"
+
+  const setClassNameForMyFormFields = function(fields, className) {
+    for (let field of fields) {
+      document.querySelector('form#' + formId + ' > input[name="' + field + '"]').className = className;
+    }
+  }
+
+  const setSuccessStatusForResultContainer = function() {
+    var container = document.getElementById('resultContainer');
+    container.innerHTML = "Success";
+    container.className = "success";
+  }
+
+  const setErrorStatusForResultContainer = function(reason) {
+    var container = document.getElementById('resultContainer');
+    container.innerHTML = reason;
+    container.className = "error";
+  }
+
+  const setProgressStatusForResultContainer = function() {
+    var container = document.getElementById('resultContainer');
+    container.innerHTML = "";
+    container.className = "progress";
+  }
+
+  const sendRequest = function() {
+    var myForm = document.getElementById(formId),
+      request = new XMLHttpRequest();
+    request.open(ajaxRequestMethod, myForm.action, true);
+    request.onload = function(event) {
+      console.log("Код статуса запроса: " + request.status);
+      if (request.status == 200 ||
+        request.status == 0) { // Это требуется для запуска страницы в Chrome с локального диска
+        // также требуется запускать Chrome с ключем --allow-file-access-from-files
+        var res = JSON.parse(request.responseText);
+        if (res.status && typeof res.status == "string") {
+          switch (res.status) {
+            case "success":
+              setSuccessStatusForResultContainer();
+              break;
+            case "error":
+              if (res.reason && typeof res.reason == "string") {
+                setErrorStatusForResultContainer(res.reason);
+              } else {
+                console.log("Ошибка: некорректное значение для поля reason.");
+              }
+              break;
+            case "progress":
+              setProgressStatusForResultContainer();
+              if (res.timeout && typeof res.timeout == "number") {
+                setTimeout(sendRequest, res.timeout.toFixed());
+              } else {
+                console.log("Ошибка: некорректное значение для поля timeout.");
+              }
+              break;
+            default:
+              console.log("Ошибка: некорректное значение для поля status.");
+              break;
+          }
+        } else {
+          console.log("Ошибка: некорректное значение для поля status.");
+        }
+      }
+    }
+    request.onerror = function(event) {
+      console.log("Ошибка: " + event.type);
+    }
+    request.send(ajaxRequestMethod == "POST" ? new FormData(myForm) : null);
+  }
+
+  const validateFio = function(fio) {
+    // Проверка поля ФИО
+    var words = fio.split(/\s+/),
+      invalids = words.filter((word) => !word.match(/(^([a-zA-Z])+$)|(^([\u0400-\u04FF])+$)/));
+    return words.length == countWordsInFio && invalids.length == 0;
+  }
+
+  const validateEmail = function(email) {
+    // Проверка поля Email
+    var template_is_matched = email.match(/^.+@[^@\s\.]+\.[^@\s\.]+$/),
+      domain_is_valid = true,
+      local_is_valid = true;
+    if (template_is_matched) {
+      var email_splited = email.split(/@/),
+        local = email_splited[0],
+        domain = email_splited[1];
+      const regex1 = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")$/;
+      local_is_valid = local.match(regex1);
+      domain_is_valid = validDomains.indexOf(domain) != -1;
+    }
+    return template_is_matched && local_is_valid && domain_is_valid;
+  }
+
+  const validatePhone = function(phone) {
+    var sum_of_digits = 0,
+      template_is_matched = phone.match(/^\+7\(\d{3}\)\d{3}\-\d{2}\-\d{2}$/);
+    if (template_is_matched) {
+      [
+        1, //       +7
+        3, 4, 5, // (999)
+        7, 8, 9, // 999
+        11, 12, //  -99
+        14, 15 //   -99
+      ].forEach((i) => sum_of_digits += parseInt(phone.charAt(i)));
+    }
+    return template_is_matched && sum_of_digits <= maxSumOfDigits;
+  }
+
+  const validateFunctions = {
+    'fio': validateFio,
+    'email': validateEmail,
+    'phone': validatePhone
+  }
+
+  // Публикуемые функции
+  const validate = function() {
+    var result = {
+        isValid: true,
+        errorFields: []
+      },
+      data = getData();
+    // Выполняем проверку полей
+    formFields.forEach((fn) => {
+      if (validateFunctions[fn] && !validateFunctions[fn](data[fn].trim())) {
+        result.errorFields.push(fn);
+        result.isValid = false;
+      }
+    });
+    // Возвращаем результат проверок в требуемой форме
+    return result;
+  }
+
+  const getData = function() {
+    let result = {};
+    formFields.forEach((fn) => {
+      const nl = document.querySelector('form#' + formId + ' > input[name="' + fn + '"]');
+      result[fn] = nl ? nl.value : null;
+    });
+    return result;
+  }
+
+  const setData = function(data) {
+    formFields.forEach((fn) => {
+      const nl = document.querySelector('form#' + formId + ' > input[name="' + fn + '"]');
+      if (nl) {
+        nl.value = data[fn] ? data[fn] : "";
+      }
+    });
+  }
+
+  const submit = function() {
+    const result = validate();
+    setClassNameForMyFormFields(formFields, null)
+    if (result.isValid) {
+      document.getElementById('submitButton').disabled = true;
+      sendRequest();
+    } else {
+      setClassNameForMyFormFields(result.errorFields, "error");
+    }
+  }
+
+  // Экспортируем объект с требуемыми методами
+  return {
+    validate: validate,
+    getData: getData,
+    setData: setData,
+    submit: submit
+  }
+})("myForm");
