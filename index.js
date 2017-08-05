87f72d381463aa9062d261c786604784774394df
kahabarov <khabarov.k.a@yandex.ru>
2017-08-05 23:55:27 +0300

    Исправлена ошибка при проверке поля email Добавлены проверки нештатных ситуаций

--- index.js
+++ index.js
@@ -12,91 +12,103 @@ const MyForm = (function(formId) {
 
   const setClassNameForMyFormFields = function(fields, className) {
     for (let field of fields) {
-      document.querySelector('form#' + formId + ' > input[name="' + field + '"]').className = className;
+      let element = document.querySelector('form#' + formId + ' input[name="' + field + '"]');
+      if (element) element.className = className;
     }
   }
 
   const setSuccessStatusForResultContainer = function() {
-    var container = document.getElementById('resultContainer');
-    container.innerHTML = "Success";
-    container.className = "success";
+    let container = document.getElementById('resultContainer');
+    if (container) {
+      container.innerHTML = "Success";
+      container.className = "success";
+    }
   }
 
   const setErrorStatusForResultContainer = function(reason) {
-    var container = document.getElementById('resultContainer');
-    container.innerHTML = reason;
-    container.className = "error";
+    let container = document.getElementById('resultContainer');
+    if (container) {
+      container.innerHTML = reason;
+      container.className = "error";
+    }
   }
 
   const setProgressStatusForResultContainer = function() {
-    var container = document.getElementById('resultContainer');
-    container.innerHTML = "";
-    container.className = "progress";
+    let container = document.getElementById('resultContainer');
+    if (container) {
+      container.innerHTML = "";
+      container.className = "progress";
+    }
   }
 
   const sendRequest = function() {
-    var myForm = document.getElementById(formId),
+    let form = document.getElementById(formId),
       request = new XMLHttpRequest();
-    request.open(ajaxRequestMethod, myForm.action, true);
+    if (!form)
+      return;
+    request.open(ajaxRequestMethod, form.action, true);
     request.onload = function(event) {
       console.log("Код статуса запроса: " + request.status);
       if (request.status == 200 ||
         request.status == 0) { // Это требуется для запуска страницы в Chrome с локального диска
         // также требуется запускать Chrome с ключем --allow-file-access-from-files
-        var res = JSON.parse(request.responseText);
-        if (res.status && typeof res.status == "string") {
-          switch (res.status) {
-            case "success":
-              setSuccessStatusForResultContainer();
-              break;
-            case "error":
-              if (res.reason && typeof res.reason == "string") {
-                setErrorStatusForResultContainer(res.reason);
-              } else {
-                console.log("Ошибка: некорректное значение для поля reason.");
-              }
-              break;
-            case "progress":
-              setProgressStatusForResultContainer();
-              if (res.timeout && typeof res.timeout == "number") {
-                setTimeout(sendRequest, res.timeout.toFixed());
-              } else {
-                console.log("Ошибка: некорректное значение для поля timeout.");
-              }
-              break;
-            default:
-              console.log("Ошибка: некорректное значение для поля status.");
-              break;
+        try {
+          let res = JSON.parse(request.responseText);
+          if (res.status && typeof res.status == "string") {
+            switch (res.status) {
+              case "success":
+                setSuccessStatusForResultContainer();
+                break;
+              case "error":
+                if (res.reason && typeof res.reason == "string") {
+                  setErrorStatusForResultContainer(res.reason);
+                } else {
+                  console.log("Ошибка: некорректное значение для поля reason.");
+                }
+                break;
+              case "progress":
+                setProgressStatusForResultContainer();
+                if (res.timeout && typeof res.timeout == "number") {
+                  setTimeout(sendRequest, res.timeout.toFixed());
+                } else {
+                  console.log("Ошибка: некорректное значение для поля timeout.");
+                }
+                break;
+              default:
+                console.log("Ошибка: некорректное значение для поля status.");
+                break;
+            }
+          } else {
+            console.log("Ошибка: некорректное значение для поля status.");
           }
-        } else {
-          console.log("Ошибка: некорректное значение для поля status.");
+        } catch (error) {
+          console.log("Ошибка: " + error.message);
         }
       }
     }
     request.onerror = function(event) {
       console.log("Ошибка: " + event.type);
     }
-    request.send(ajaxRequestMethod == "POST" ? new FormData(myForm) : null);
+    request.send(ajaxRequestMethod == "POST" ? new FormData(form) : null);
   }
 
   const validateFio = function(fio) {
     // Проверка поля ФИО
-    var words = fio.split(/\s+/),
+    let words = fio.split(/\s+/),
       invalids = words.filter((word) => !word.match(/(^([a-zA-Z])+$)|(^([\u0400-\u04FF])+$)/));
     return words.length == countWordsInFio && invalids.length == 0;
   }
 
   const validateEmail = function(email) {
     // Проверка поля Email
-    var template_is_matched = email.match(/^.+@[^@\s\.]+\.[^@\s\.]+$/),
+    let template_is_matched = email.match(/^(.+)@([^@\s\.]+\.[^@\s\.]+)$/),
       domain_is_valid = true,
       local_is_valid = true;
     if (template_is_matched) {
-      var email_splited = email.split(/@/),
-        local = email_splited[0],
-        domain = email_splited[1];
-      const regex1 = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")$/;
-      local_is_valid = local.match(regex1);
+      let local = template_is_matched[1],
+        domain = template_is_matched[2];
+      const re_local = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")$/;
+      local_is_valid = local.match(re_local) != null;
       domain_is_valid = validDomains.indexOf(domain) != -1;
     }
     return template_is_matched && local_is_valid && domain_is_valid;
@@ -125,7 +137,7 @@ const MyForm = (function(formId) {
 
   // Публикуемые функции
   const validate = function() {
-    var result = {
+    let result = {
         isValid: true,
         errorFields: []
       },
@@ -144,7 +156,7 @@ const MyForm = (function(formId) {
   const getData = function() {
     let result = {};
     formFields.forEach((fn) => {
-      const nl = document.querySelector('form#' + formId + ' > input[name="' + fn + '"]');
+      const nl = document.querySelector('form#' + formId + ' input[name="' + fn + '"]');
       result[fn] = nl ? nl.value : null;
     });
     return result;
@@ -152,7 +164,7 @@ const MyForm = (function(formId) {
 
   const setData = function(data) {
     formFields.forEach((fn) => {
-      const nl = document.querySelector('form#' + formId + ' > input[name="' + fn + '"]');
+      const nl = document.querySelector('form#' + formId + ' input[name="' + fn + '"]');
       if (nl) {
         nl.value = data[fn] ? data[fn] : "";
       }
